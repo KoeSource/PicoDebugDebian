124c9274b89c52a795f336af5efe846f2deb3a37
Koe <pc_frik@bluewin.ch>
2024-03-19 16:09:22 +0100

    upload.bash filesearch added, no manual edit needed anymore

--- upload.bash
+++ upload.bash
@@ -1,6 +1,18 @@
  #!/bin/bash
  # Uploads your Project to RP2040 through PicoProbe
 
-file="pdd.elf"
+regex='^build\/.+.elf$'
 
-openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "program build/$file verify reset exit"
+files=$(find build -maxdepth 1  -regex "$regex" -printf "%f\n")
+files_count=$(echo -n "$files" | grep -c '^')
+
+if [[ $files_count -gt 1 ]]; then
+  echo "Multiple files found:"
+  echo "$files"
+  echo "Upload canceled!"
+elif [[ $files_count -lt 1 ]]; then
+  echo "No files found to upload"
+  echo "example: build/*.elf"
+else
+  openocd -f interface/cmsis-dap.cfg -f target/rp2040.cfg -c "program build/$files verify reset exit"
+fi
